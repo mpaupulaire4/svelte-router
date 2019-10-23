02222a371852887033605274e7ec8978f1308842
Michel Paupulaire <mpaupulaire4@gmail.com>
2019-10-23 08:45:52 -0700

    Save

--- src/Route.svelte
+++ src/Route.svelte
@@ -12,21 +12,22 @@
 
   const { segment, base } = getContext('svelte-router')
   const newSegment = `${absolute ? '' : segment}/${path.replace(/^\//, '')}`
-  const pattern = new URLPattern(newSegment)
-  const params = derived(route, ($route) => pattern.match(`${base}${$route}`));
+  const pattern = new URLPattern(`${base}${newSegment}`)
   setContext('svelte-router', {
     base,
-    params,
     segment: newSegment
   })
 
+  $: params = pattern.match($route);
+  $: promise = params ? preload({ params, query: $query, path: $route }) : {};
+
 </script>
-{#if $params}
-  {#await preload({params: $params, query: $query, path: $route}) then data}
+{#if params}
+  {#await promise then data}
     {#if component}
-      <svelte:component this="{component}" params="{$params}" query="{$query}" route="{$route}" {...data} />
+      <svelte:component this="{component}" params="{params}" query="{$query}" route="{$route}" {...data} />
     {:else}
-      <slot params="{$params}" query="{$query}" route="{$route}" {data} />
+      <slot params="{params}" query="{$query}" route="{$route}" {data} />
     {/if}
   {/await}
 {/if}
