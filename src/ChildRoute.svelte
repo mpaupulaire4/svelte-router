a631d4497de6085540aa53ed2766cf16b4ffa29b
Michel Paupulaire <mpaupulaire4@gmail.com>
2020-12-14 12:20:40 -0500

    ChildRoute change

--- src/ChildRoute.svelte
+++ src/ChildRoute.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
   import type { Readable } from 'svelte/store'
-  import type { ActiveHandler } from './Router'
+  import type { Handlers } from './Router.svelte'
   import { getContext, setContext } from "svelte";
 
-  const handlers = getContext<Readable<ActiveHandler[]>>('svelte-router-internal-handlers')
+  const handlers = getContext<Readable<Handlers>>('svelte-router-internal-handlers')
   const index = getContext<number>('svelte-router-internal-index') || 0
   setContext('svelte-router-internal-index', index + 1)
 
-  $: handler = $handlers[index] || { component: null, data: {} }
+  $: handlerP = $handlers[index] || { component: null, data: {} }
 
 </script>
 
+{#await handlerP}
+<slot />
+{:then handler}
 <svelte:component this="{handler.component}" {...handler.data} {...$$props}>
   <svelte:self/>
 </svelte:component>
+{/await}
