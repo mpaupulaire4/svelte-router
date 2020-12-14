e13f7dd5a9e47c93e0174e350dee2cdf533ecb22
Michel Paupulaire <mpaupulaire4@gmail.com>
2020-12-14 12:27:52 -0500

    cleanup

---
+++ test/integratoin/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import {Router} from '../../src'
+</script>
+
+<main>
+	<div>
+		<a href="/?some=thing&some=thing2">Index</a>
+	</div>
+	<div>
+		<a href="/post">Post Index</a>
+	</div>
+	<div>
+		<a href="/post/new">Post New</a>
+	</div>
+	<div>
+		<a href="/post/1">Post 1</a>
+	</div>
+
+	<Router>
+
+	</Router>
+</main>
