cb0edcc3b78f2306b7c242f906273edf94664258
Michel Paupulaire <mpaupulaire4@gmail.com>
2020-12-18 10:06:39 -0500

    Add testing in CI

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 const svelte = require('rollup-plugin-svelte');
 const { nodeResolve: resolve } = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
-const livereload = require('rollup-plugin-livereload');
 import { terser } from 'rollup-plugin-terser';
 const typescript = require('@rollup/plugin-typescript');
 const config = require('./svelte.config');
@@ -9,35 +8,9 @@ const pkg = require('./package.json')
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev', '--single'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
-
 module.exports = {
-	input: !production ? 'test/live/main.ts' : 'src/index.ts',
-	output: !production ? {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	} : [
+	input: 'src/index.ts',
+	output: [
 		{ file: pkg.module, format: 'es' },
 		{ file: pkg.main, format: 'umd', name: 'SvelteRouter', plugins: [terser()] },
 	],
@@ -55,14 +28,6 @@ module.exports = {
 		}),
 		commonjs(),
 		typescript({ sourceMap: !production }),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
 	],
 	watch: {
 		clearScreen: false
